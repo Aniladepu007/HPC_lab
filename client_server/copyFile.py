5b1ca3b9bccd1c9e59897946e29e0e599116c00e
aniladepu007 <ced16i002@iiitdm.ac.in>
2019-08-29 14:22:25 +0530

    5th

--- client_server/copyFile.py
+++ client_server/copyFile.py
@@ -1,3 +1,3 @@
 with open("clientFile.txt","w") as f:
-    for i in range(500000):
-        f.write("HelloWorld !!!")
+    for i in range(50000):
+        f.write("Hello World, This file size is > 1MB\n")
--- client_server/thread_server.cpp
+++ client_server/thread_server.cpp
@@ -47,7 +47,7 @@ int32_t main(int count, char* arg[]) {
       sockfd = socket(AF_INET, SOCK_STREAM, 0);
       struct sockaddr_in server_addr;
       server_addr.sin_family = AF_INET;
-      server_addr.sin_addr.s_addr = inet_addr("192.168.43.243"; //INADDR_ANY;
+      server_addr.sin_addr.s_addr = inet_addr("192.168.43.243"); //INADDR_ANY;
       server_addr.sin_port = htons(portNo);
 
       bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
