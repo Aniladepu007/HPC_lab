9fefc9f432751800be027e5f08c6960547f94f11
aniladepu007 <ced16i002@iiitdm.ac.in>
2019-11-13 11:21:02 +0530

    nov13EndSem

--- client_server/s0.cpp
+++ client_server/s0.cpp
@@ -41,7 +41,7 @@ void runner() {
 }
 
 int32_t main() {
-      ll sockfd,portNo = 9999;
+      ll sockfd,portNo = 9898;
       sockfd = socket(AF_INET, SOCK_STREAM, 0);
       struct sockaddr_in server_addr;
       server_addr.sin_family = AF_INET;
